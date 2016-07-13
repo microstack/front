63a34f44a00f1eeb79bb5d71550a951971570cd8
wikibootup <wikibootup@gmail.com>
2016-07-14 00:39:04 +0900

    add stub movie, actor mainpage url, view in each

--- front/views.py
+++ front/views.py
@@ -1,6 +1,16 @@
+from flask import render_template
+
 from . import app
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/movies/')
+def movie_mainpage():
+    return 'Movie mainpage'
+
+@app.route('/actors/')
+def actor_mainpage():
+    return 'Actor mainpage'
