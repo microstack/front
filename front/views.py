2153e8140faaedd5fe35521d337bb47ceae8d75f
wikibootup <wikibootup@gmail.com>
2016-07-29 21:50:47 +0900

    remove /movies/ service from front

--- front/views.py
+++ front/views.py
@@ -10,15 +10,6 @@ import requests
 def index():
     return render_template('index.html')
 
-@app.route('/movies/')
-def movie_mainpage():
-    url = os.environ.get('API_GW_URL')
-    port = os.environ.get('API_GW_PORT')
-    response = requests.get(url + ':' + port + '/movies/')
-    text = response.text
-
-    return text
-
 @app.route('/actors/')
 def actor_mainpage():
     return 'Actor mainpage'
