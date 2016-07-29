5777e10775207a95a5e7d0c9dee72e2d976ff3ba
wikibootup <wikibootup@gmail.com>
2016-07-30 00:51:24 +0900

    seperate template from view

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/movies/')
 def movie_mainpage():
-    return "Hello movies!"
+    return render_template('movies.html')
