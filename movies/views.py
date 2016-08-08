232fbb29ae6a5416b27d0ae8d91e79ed9d910042
wikibootup <wikibootup@gmail.com>
2016-08-09 03:06:38 +0900

    remove self arg in function

--- movies/views.py
+++ movies/views.py
@@ -9,7 +9,7 @@ from settings import API_GW_BASE_URL
 app = Flask(__name__)
 
 
-def response_text_from_request(self, base_url, resource):
+def response_text_from_request(base_url, resource):
     response = requests.get(base_url + resource)
     text = response.text
     return text
