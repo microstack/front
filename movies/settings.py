0b5ba7ae6fb8b613cf8b4b515b0d53295e9c46c4
wikibootup <wikibootup@gmail.com>
2016-08-09 02:56:07 +0900

    movie api from api gw

---
+++ movies/settings.py
@@ -0,0 +1,4 @@
+import os
+
+# ex :) API_GW_BACKEND_BASE_URL = 'http://localhost:8080'
+API_GW_BASE_URL = os.environ.get('API_GW_BASE_URL')
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,18 @@
+import requests
+import os
+
 from flask import render_template
 from flask import Flask
-app = Flask(__name__)
+
+from settings import API_GW_BASE_URL
+
+def response_text_from_request(self, base_url, resource):
+    response = requests.get(base_url + resource)
+    text = response.text
+    return text
+
 
 @app.route('/movies/')
 def movie_mainpage():
-    return render_template('movies.html')
+    text = response_text_from_request(API_GW_BASE_URL, '/movies/')
+    return text
