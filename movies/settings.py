88e93cdc7d5f9b5d381d51ec00f7d996d80e3a8e
wikibootup <wikibootup@gmail.com>
2016-08-11 23:59:04 +0900

    add local env ports in wsgi

--- movies/settings.py
+++ movies/settings.py
@@ -1,4 +1,6 @@
 import os
 
 # ex :) API_GW_BACKEND_BASE_URL = 'http://localhost:8080'
-API_GW_BASE_URL = os.environ.get('API_GW_BASE_URL')
+API_GW_BASE_URL = os.environ.get('API_GW_BASE_URL') or 'http://localhost'
+FRONT_MAIN_PORT = os.environ.get('FRONT_MAIN_PORT') or 5000
+FRONT_MOVIES_PORT = os.environ.get('FRONT_MOVIES_PORT') or 5000
--- movies/wsgi.py
+++ movies/wsgi.py
@@ -1,4 +1,5 @@
 from views import app
+from settings import FRONT_MOVIES_PORT
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=FRONT_MOVIES_PORT)
