e60c4a7c5ae0ed058776378083a05725ba3d46e0
wikibootup <wikibootup@gmail.com>
2016-08-20 15:26:03 +0900

    apply bootstrap css, need to delete .map

--- movies/views.py
+++ movies/views.py
@@ -15,7 +15,12 @@ def response_text_from_request(base_url, resource):
     return text
 
 
-@app.route('/movies/')
-def movie_mainpage():
+@app.route('/movies/archive/')
+def movie_archive():
     text = response_text_from_request(API_GW_BASE_URL, '/movies/')
     return text
+
+
+@app.route('/movies/')
+def movie_mainpage():
+    return render_template('index.html')
