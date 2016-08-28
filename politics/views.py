de16b81784ef519dd78bcfcc92af342efa1f4958
wikibootup <wikibootup@gmail.com>
2016-08-28 18:37:46 +0900

    add post (bill detail)

--- politics/views.py
+++ politics/views.py
@@ -16,9 +16,22 @@ def response_text_from_request(base_url, resource):
 
 
 @app.route('/politics/bill/')
-def politic_mainpage():
-    bill_text = response_text_from_request(API_GW_BASE_URL, '/politics/bill/')
+def bill_list():
+    resource = '/politics/bill/'
+    bill_text = response_text_from_request(API_GW_BASE_URL, resource)
     items = json.loads(bill_text)
     bill_list = items['items']
 
     return render_template('index.html', bill_list=bill_list)
+
+
+@app.route('/politics/bill/<int:id>/')
+def bill_detail(id):
+    resource = '/politics/bill/%s' % id
+    bill_text = response_text_from_request(API_GW_BASE_URL, resource)
+    bill = json.loads(bill_text)
+    _, summary_reason, summary_main = bill['summary'].split('■')
+    bill['summary_main'] = summary_main
+    bill['summary_reason'] = summary_reason
+
+    return render_template('post.html', bill=bill)
