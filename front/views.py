78b45447e835de8900ac01c093781721fab61f8d
wikibootup <wikibootup@gmail.com>
2016-08-28 20:42:15 +0900

    add todo about service list health check

--- front/views.py
+++ front/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 
 from flask import render_template
@@ -8,6 +11,11 @@ import requests
 
 @app.route('/')
 def index():
+
+    '''
+    서비스 목록 url을 받아서 http 요청이 성공하는지, 텍스트에 error가 포함되지
+    않았는지 확인
+    '''
     services = []
     for i in range(5):
         services.append(
