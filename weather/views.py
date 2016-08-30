1ae50caeb71f37b0150f35e3ef0e879425ef243f
wikibootup <wikibootup@gmail.com>
2016-08-30 19:06:03 +0900

    apply today api in front

--- weather/views.py
+++ weather/views.py
@@ -10,11 +10,46 @@ app = Flask(__name__)
 
 
 def response_text_from_request(base_url, resource):
-    response = requests.get(base_url + resource)
+    try:
+        response = requests.get(base_url+resource)
+    except requests.ConnectionError:
+        text = "{'error': 'ConnectionError'}"
+        return text
+
+    if response.status_code != 200:
+        text = "{'error': 'Statuscode : %s' % response.status_code}"
+        return text
+
     text = response.text
     return text
 
 
+def objects_from_request(base_url, resource):
+    text = response_text_from_request(base_url, resource)
+    objects = json.loads(text)
+    return objects
+
+
+def is_error_in_objects(objects):
+    for key in objects.keys():
+        if isinstance(objects[key], (list, str)):
+            continue
+
+        if objects[key].get('error'):
+            return True
+    return False
+
+
 @app.route('/weather/')
 def weather_mainpage():
-    return render_template('index.html')
+    objects = dict()
+
+    today_weather = objects_from_request(API_GW_BASE_URL,
+        '/weather/today/weather')
+    today_publish = objects_from_request(API_GW_BASE_URL,
+        '/weather/today/')
+
+    objects['weather'] = today_weather
+    objects['publish'] = today_publish
+    
+    return render_template('index.html', objects=objects)
